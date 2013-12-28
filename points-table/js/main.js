f8d9b55638934792e4bd972e50778bd2a3f06223
Till Helge Helwig <till.helwig@razorfish.de>
2013-12-28 12:41:35 +0100

    Minor code cleanup,

--- points-table/js/main.js
+++ points-table/js/main.js
@@ -26,32 +26,32 @@ var pt_init = function () {
 };
 
 var pt_setPlayerCount = function (event) {
+    var i = 1,
+        numberOfPlayers = jQuery('#number_of_players').val();
     jQuery('#step1').hide();
-    var numberOfPlayers = jQuery('#number_of_players').val();
     currentPlayerIndex = numberOfPlayers;
-    for (var i = 1; i <= numberOfPlayers; ++i) {
-        var newRow = document.createElement('tr');
-        var newTd = document.createElement('td');
+    for (i = 1; i <= numberOfPlayers; ++i) {
+        var newRow = document.createElement('tr'),
+            newTd = document.createElement('td');
         newTd.textContent = i;
         jQuery(newRow).append(newTd);
         newTd = document.createElement('td');
-        var newInput = document.createElement('input');
-        if (i < numberOfPlayers) {
-            jQuery(newInput).on('keypress', function (event) {
-                if (event.keyCode == 13) {
-                    var parentTr = jQuery(event.target).parent('td').parent('tr').next();
-                    var targetInput = jQuery(parentTr).children('td:nth-child(2)').children('input').first();
-                    jQuery(targetInput).focus();
-                }
-            });
-        } else {
-            jQuery(newInput).on('keypress', function (event) {
+        var newInput = document.createElement('input'),
+            keypressFunction = function (event) {
                 if (event.keyCode == 13) {
                     pt_setPlayerNames();
                 }
-            });
+            };
+        if (i < numberOfPlayers) {
+            keypressFunction = function (event) {
+                if (event.keyCode == 13) {
+                    var parentTr = jQuery(event.target).parent('td').parent('tr').next(),
+                        targetInput = jQuery(parentTr).children('td:nth-child(2)').children('input').first();
+                    jQuery(targetInput).focus();
+                }
+            };
         }
-        jQuery(newInput).attr('type', 'text');
+        jQuery(newInput).attr('type', 'text').on('keypress', keypressFunction);
         playerNames.push(newInput);
         playerPoints.push(0);
         jQuery(newRow).append(newTd);
@@ -63,12 +63,13 @@ var pt_setPlayerCount = function (event) {
 };
 
 var pt_setPlayerNames = function (event) {
+    var i = 0,
+        actualPlayerNames = [];
     jQuery('#step2').hide();
-    var actualPlayerNames = [];
-    for (var i = 0; i < playerNames.length; ++i) {
-        var playerName = jQuery(playerNames[i]).val();
+    for (i = 0; i < playerNames.length; ++i) {
+        var playerName = jQuery(playerNames[i]).val(),
+            newTh = document.createElement('th');
         actualPlayerNames.push(playerName);
-        var newTh = document.createElement('th');
         newTh.textContent = playerName;
         jQuery('#step4 table#points_table thead #player_names').append(newTh);
         newTh = document.createElement('th');
@@ -86,11 +87,11 @@ var pt_nextPointsInput = function (event) {
     if (currentPlayerIndex >= playerNames.length) {
         roundNumber++;
         currentPlayerIndex = 0;
-        var newRow = document.createElement('tr');
-        var targetElement = jQuery('#step4 table#points_table tbody');
+        var newRow = document.createElement('tr'),
+            targetElement = jQuery('#step4 table#points_table tbody'),
+            newTd = document.createElement('td');
         jQuery(targetElement).append(newRow);
         currentTableRow = newRow;
-        var newTd = document.createElement('td');
         newTd.textContent = roundNumber;
         jQuery(currentTableRow).append(newTd);
     }
@@ -114,7 +115,8 @@ var pt_enterPlayerNoPoints = function (event) {
 };
 
 var pt_addPlayerPoints = function (points) {
-    var newTd = document.createElement('td');
+    var newTd = document.createElement('td'),
+        totalPointThs = jQuery('#step4 table#points_table thead tr#total_points th');
     newTd.textContent = points;
     jQuery(newTd).attr('data-round', roundNumber);
     jQuery(newTd).attr('data-player', currentPlayerIndex);
@@ -122,7 +124,6 @@ var pt_addPlayerPoints = function (points) {
     jQuery(newTd).on('click', pt_editPoints);
     jQuery(currentTableRow).append(newTd);
     playerPoints[currentPlayerIndex] += points;
-    var totalPointThs = jQuery('#step4 table#points_table thead tr#total_points th');
     totalPointThs[currentPlayerIndex + 1].textContent = playerPoints[currentPlayerIndex];
     pt_highlightLeader();
     if (!jQuery('#step4').is(':visible')) {
@@ -137,18 +138,18 @@ var pt_editPoints = function (event) {
     jQuery(newInput).attr('type', 'number');
     jQuery(newInput).val(event.target.textContent);
     jQuery(newInput).on('keypress', function (event) {
-            var targetTd = jQuery(event.target).parent();
-            var oldValue = jQuery(targetTd).attr('data-value');
-            if (event.keyCode == 13) {
-                var newValue = jQuery(event.target).val();
-                jQuery(targetTd).empty().text(newValue).attr('data-value', newValue);
-                var playerIndex = parseInt(jQuery(targetTd).attr('data-player'));
-                playerPoints[playerIndex] += (newValue - oldValue);
-                var totalPointsThs = jQuery('#step4 table#points_table thead tr#total_points th');
-                totalPointsThs[playerIndex + 1].textContent = playerPoints[playerIndex];
-                pt_highlightLeader();
-                jQuery('#points').focus();
-            }
+        var targetTd = jQuery(event.target).parent(),
+            oldValue = jQuery(targetTd).attr('data-value'),
+            newValue = jQuery(event.target).val();
+        if (event.keyCode == 13) {
+            jQuery(targetTd).empty().text(newValue).attr('data-value', newValue);
+            var playerIndex = parseInt(jQuery(targetTd).attr('data-player')),
+                totalPointsThs = jQuery('#step4 table#points_table thead tr#total_points th');
+            playerPoints[playerIndex] += (newValue - oldValue);
+            totalPointsThs[playerIndex + 1].textContent = playerPoints[playerIndex];
+            pt_highlightLeader();
+            jQuery('#points').focus();
+        }
     });
     jQuery(newInput).on('blur', function (event) {
         var oldValue = jQuery(event.target).parent('td').attr('data-value');
@@ -160,10 +161,12 @@ var pt_editPoints = function (event) {
 };
 
 var pt_highlightLeader = function () {
-    var highestPoints = 0;
-    var leadingPlayerIndexes = [];
-    var pointsMap = [];
-    for (var i = 0; i < playerPoints.length; ++i) {
+    var highestPoints = 0,
+        leadingPlayerIndexes = [],
+        pointsMap = [],
+        i = 0,
+        totalPointThs = jQuery('#step4 table#points_table thead tr#total_points th');
+    for (i = 0; i < playerPoints.length; ++i) {
         pointsMap.push({name: playerNames[i], points: playerPoints[i]});
         if (playerPoints[i] > highestPoints) {
             leadingPlayerIndexes = [i];
@@ -173,24 +176,28 @@ var pt_highlightLeader = function () {
             leadingPlayerIndexes.push(i);
         }
     }
-    var totalPointThs = jQuery('#step4 table#points_table thead tr#total_points th');
     jQuery(totalPointThs).removeClass('leader');
-    for (var j = 0; j < leadingPlayerIndexes.length; ++j) {
-        jQuery(totalPointThs[leadingPlayerIndexes[j] + 1]).addClass('leader');
+    for (i = 0; i < leadingPlayerIndexes.length; ++i) {
+        jQuery(totalPointThs[leadingPlayerIndexes[i] + 1]).addClass('leader');
     }
     pointsMap.sort(function (a, b) {
         return b.points - a.points;
     });
+    pt_updateRankingTable(pointsMap);
+};
+
+var pt_updateRankingTable = function (pointsMap) {
     jQuery('#ranking table#ranking_table tbody').empty();
-    var rank = 0;
-    var currentRankPoints = -1;
-    for (var i = 1; i <= pointsMap.length; ++i) {
+    var rank = 0,
+        currentRankPoints = -1,
+        i = 1;
+    for (i = 1; i <= pointsMap.length; ++i) {
         if ((currentRankPoints > pointsMap[i - 1].points) || (currentRankPoints < 0)) {
             rank++;
             currentRankPoints = pointsMap[i - 1].points;
         }
-        var newTr = document.createElement('tr');
-        var newTd = document.createElement('td');
+        var newTr = document.createElement('tr'),
+            newTd = document.createElement('td');
         newTd.textContent = rank + '.';
         jQuery(newTr).append(newTd);
         newTd = document.createElement('td');
